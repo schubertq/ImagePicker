8ec4ae8aea4590cc582e03661884ad96424f0374
PeterShu <878393556@qq.com>
2018-07-17 20:07:19 +0800

    master 1

--- ImagePicker/src/app/app.component.ts
+++ ImagePicker/src/app/app.component.ts
@@ -12,6 +12,7 @@ import { TabsPage } from '../pages/tabs/tabs';
 export class MyApp {
   rootPage = TabsPage;
 
+  master 1
   constructor(platform: Platform) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
